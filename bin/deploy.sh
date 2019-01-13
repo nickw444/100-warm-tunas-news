b788bc40b0d63036800eecba8335ac16c5bcbfd5
Travis CI <travis@travis-ci.org>
2019-01-13 21:59:37 +1100

    .

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ function main() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
     git clone https://${GITHUB_TOKEN}@github.com/nickw444/100-warm-tunas.git
-    cp -r _site/ 100-warm-tunas/news/
+    cp -r _site/news/ 100-warm-tunas/news/
 
     pushd 100-warm-tunas >/dev/null
     git add news/
