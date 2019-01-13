c83e17c54cfcc40cbfd663511d0e0d90415550d6
Nick Whyte <nick@nickwhyte.com>
2019-01-13 23:02:27 +1100

    Clear news dir on deploy + ignore bin

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,7 @@ main() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
     git clone https://${GITHUB_TOKEN}@github.com/nickw444/100-warm-tunas.git
+    rm -rf 100-warm-tunas/news
     cp -r _site/news/ 100-warm-tunas/news/
 
     cd 100-warm-tunas
