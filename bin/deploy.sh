75e79319c971f7dc1bda6422dc5af6310ba4eb0e
Nick Whyte <nick@nickwhyte.com>
2019-01-13 22:16:33 +1100

    .

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ main() {
     git clone https://${GITHUB_TOKEN}@github.com/nickw444/100-warm-tunas.git
     cp -r _site/news/ 100-warm-tunas/news/
 
-    pushd 100-warm-tunas >/dev/null
+    cd 100-warm-tunas
     git add news/
     git commit -m "Travis build: ${TRAVIS_BUILD_NUMBER}"
     git push origin master
